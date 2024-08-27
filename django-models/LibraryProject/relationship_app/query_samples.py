c0e1a649a7e2dc1f8d339fb2c7640eb51569eae8
ericmuni-23 <munieric44@gmail.com>
2024-08-28 01:25:53 +0300

    Add queries to Django app

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -2,7 +2,7 @@ from relationship_app import Author, Book, Library, Librarian
 
 def get_books_by_author(author_name):
     author = Author.objects.get(name=author_name)
-    books = Author.books.all()
+    books = Book.objects.filter(author=author)
     return books
 
 def get_books_in_library(library_name):
