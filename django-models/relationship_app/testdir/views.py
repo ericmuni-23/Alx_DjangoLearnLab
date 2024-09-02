4d1a9023c19e063d4b62da5ba8ccf1705cf505b5
ericmuni-23 <munieric44@gmail.com>
2024-09-02 23:19:11 +0300

    Add templates for user login, sign up and logout

--- django-models/relationship_app/testdir/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-from .models import Book
-def list_books(request):
-    books = Book.objects.all()
-    return render (request, 'list_books.html',{'books':books})
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -3,6 +3,11 @@ from django.shortcuts import render
 from .models import Book
 from .models import Library
 
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
+
 def list_books(request):
     books = Book.objects.all()
     return render (request, 'relationship_app/list_books.html',{'books':books})
@@ -11,3 +16,29 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')  # Redirect to home or any page after login
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return render(request, 'logout.html')
