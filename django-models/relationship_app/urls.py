bb6257c373275eb7682ad2a48de53b492eac6400
ericmuni-23 <munieric44@gmail.com>
2024-09-06 01:15:32 +0300

    A simple code to manage different user roles and permissions effectively

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -4,6 +4,8 @@ from django.urls import path
 from .views import list_books, LibraryDetailView
 from . import views
 from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import path
+from . import view
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
@@ -12,3 +14,7 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
 ]
+urlpatterns = [
+    path('admin/', views.admin_view, name='admin_view'),
+    path('librarian/', views.librarian_view, name='librarian_view'),
+    path('member/', views.member_view, name='member_view'),]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -2,11 +2,17 @@ from django.views.generic.detail import DetailView
 from django.shortcuts import render
 from .models import Book
 from .models import Library
-
 from django.contrib.auth import login, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import render
+
 
 def list_books(request):
     books = Book.objects.all()
@@ -42,3 +48,34 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return render(request, 'logout.html')
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=20, choices=['Admin', 'Librarian', 'Member'])
+
+    def __str__(self):
+        return self.user.username
+    #reciever(post_save,sender=User)   
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objectscreate(user=instance)
+
+def admin_view(request):
+    if request.user.userprofile.role == 'Admin':
+        return render(request, 'admin.html')
+    else:
+        return render(request, 'access_denied.html')
+
+def librarian_view(request):
+    if request.user.userprofile.role == 'Librarian':
+        return render(request, 'librarian.html')
+    else:
+        return render(request, 'access_denied.html')
+
+def member_view(request):
+    if request.user.userprofile.role == 'Member':
+        return render(request, 'member.html')
+    else:
+        return render(request, 'access_denied.html')
+    
+
