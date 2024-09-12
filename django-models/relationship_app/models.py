b6a1e409a68f68018c4c59ba8a44b9ce4baf1324
ericmuni-23 <munieric44@gmail.com>
2024-09-12 22:33:13 +0300

    Add classes to create role views

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -45,5 +46,5 @@ class UserProfile(models.Model):
     role = models.CharField(max_length=10, choices=ROLE_CHOICES)
 
     def __str__(self):
-        return f"{self.user.username} - {self.role}"
+        return self.username
     
