94a39f73f0aa57d853d9d2b66d2ae27496bcb2db
ericmuni-23 <munieric44@gmail.com>
2024-09-12 14:33:25 +0300

    Add templates to setting dir

---
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -0,0 +1,20 @@
+# relationship_app/urls.py
+
+from django.urls import path
+from .views import list_books, LibraryDetailView
+from . import views
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import path
+from . import view
+
+urlpatterns = [
+    path('books/', list_books, name='list_books'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('register/', views.register, name='register'),
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
+]
+urlpatterns = [
+    path('admin/', views.admin_view, name='admin_view'),
+    path('librarian/', views.librarian_view, name='librarian_view'),
+    path('member/', views.member_view, name='member_view'),]
