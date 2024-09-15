46bfa05e0dec71af7a30747dc5ac1b873d49955f
ericmuni-23 <munieric44@gmail.com>
2024-09-15 23:03:09 +0300

    Update Django’s user model

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,4 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
+    def __str__(self):
+        return self.username
 
 class Author(models.Model):
     name=models.CharField(max_length=215)
@@ -26,3 +35,12 @@ class Librarian(models.Model):
 
     def __str__(self):
             return self.name
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.username
