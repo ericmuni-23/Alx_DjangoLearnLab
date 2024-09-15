f25ee674f71a9990c935915d89af1dc1e62a86b0
ericmuni-23 <munieric44@gmail.com>
2024-09-16 00:25:37 +0300

    A simple code that utilizes groups and permissions to restrict access to certain parts of an app

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,34 +2,25 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.contrib.auth.decorators import permission_required
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponseForbidden
-from .models import Document
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Article
 
 @permission_required('app_name.can_view', raise_exception=True)
-def document_list(request):
-    documents = Document.objects.all()
-    return render(request, 'app_name/document_list.html', {'documents': documents})
+def view_article(request, article_id):
+    article = get_object_or_404(Article, id=article_id)
+    return render(request, 'view_article.html', {'article': article})
 
 @permission_required('app_name.can_create', raise_exception=True)
-def document_create(request):
-    if request.method == 'POST':
-        # Handle document creation logic here
-        pass
-    return render(request, 'app_name/document_form.html')
+def create_article(request):
+    # Implementation for creating an article
+    pass
 
 @permission_required('app_name.can_edit', raise_exception=True)
-def document_edit(request, pk):
-    document = get_object_or_404(Document, pk=pk)
-    if request.method == 'POST':
-        # Handle edit logic here
-        pass
-    return render(request, 'app_name/document_form.html', {'document': document})
+def edit_article(request, article_id):
+    # Implementation for editing an article
+    pass
 
 @permission_required('app_name.can_delete', raise_exception=True)
-def document_delete(request, pk):
-    document = get_object_or_404(Document, pk=pk)
-    if request.method == 'POST':
-        document.delete()
-        return redirect('document_list')
-    return render(request, 'app_name/document_confirm_delete.html', {'document': document})
+def delete_article(request, article_id):
+    # Implementation for deleting an article
+    pass
