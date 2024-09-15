f243be77ec0236cf240860a9ad6894c4241e976f
ericmuni-23 <munieric44@gmail.com>
2024-09-15 23:08:15 +0300

    Update Django’s user model

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Book
+from django.contrib import admin
+from .models import CustomUser
+
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'date_of_birth', 'profile_photo')
+
+admin.site.register(CustomUser, CustomUserAdmin)
 
 # Register your models here.
 class BookAdmin(admin.ModelAdmin):
